c280b54670cd93d40c114258db7cac4957d714a0
Vladyslav <vladoromus@gmail.com>
2025-06-12 23:25:00 +0200

    solution2

--- db/models.py
+++ db/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 
+
 class Race(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True)
@@ -9,7 +10,11 @@ class Race(models.Model):
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
     bonus = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name='skills')
+    race = models.ForeignKey(
+        Race,
+        on_delete=models.CASCADE,
+        related_name="skills"
+    )
 
 
 class Guild(models.Model):
