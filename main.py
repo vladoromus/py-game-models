5f0b646ed67197bc849c79e7d5b0dd1f8c5e8dd9
Vladyslav <vladoromus@gmail.com>
2025-06-12 23:29:05 +0200

    solution4

--- main.py
+++ main.py
@@ -14,10 +14,8 @@ def main() -> None:
         for skill_data in pdata["race"].get("skills", []):
             Skill.objects.get_or_create(
                 name=skill_data["name"],
-                defaults={
-                    "bonus": skill_data["bonus"],
-                    "race": race
-                }
+                race=race,
+                defaults={"bonus": skill_data["bonus"]}
             )
 
         guild_data = pdata.get("guild")
